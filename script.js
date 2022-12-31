0b89d8c2575c5a30c53a514c227b1027a3bbc681
DeekshaTripathi7 <deeksha18495@gmail.com>
2022-12-31 13:14:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+document.body.innerHTML = `
+<div class='container'>
+    <div class = 'row' id = 'content'>
+    
+    </div>
+</div>
+
+`;
+
+//get Rest API data
+async function getRestCountryData() {
+  try {
+    //fetch data from Rest Api
+    let url =
+      "https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json";
+    const data = await fetch(url);
+    const jsonData = await data.json();
+    return jsonData;
+  } catch (error) {
+    content.innerHTML =
+      "Oops !! Something went wrong. Sorry for the inconvenience";
+  }
+}
+
+//calling the async function
+let allCountryData = getRestCountryData();
+console.log(allCountryData);
+allCountryData.then((allData) => {
+  for (let i in allData) {
+    displayData(allData[i]);
+  }
+});
+
+const displayData = (countryData) => {
+  content.innerHTML += `
+    <div class = 'col-lg-4'>
+    <div class = 'card text-center' style="width: 25rem;">
+    <div class = 'card-header'>
+        <h4 class = 'countryName'>${countryData.name}</h4>
+    </div>
+    <img class = 'countryFlag' src = '${countryData.flag}' alt = '${countryData.name} Flag'>
+    <div class = 'card-body'>
+        <p class = 'capital'> Capital: <span> ${countryData.capital}</span> </p>
+        <p class = 'region'> Region: <span> ${countryData.region}</span> </p>
+        <p class = 'latlng'> Latlng: <span> ${countryData.latlng}</span> </p>
+        <p class = 'code'> CountryCode: <span> ${countryData.cioc}</span> </p>
+        <button class = 'btn btn-outline-dark' id = 'weatherBtn' onClick = 'checkWeather(${countryData.latlng[0]},${countryData.latlng[1]})'>
+            Click for Weather         <p id = 'temp'> Temperature </p>
+
+        </button>
+    </div>
+    </div>
+    </div>
+    `;
+};
+
+//fetching temperature from OpenWeather API
+const checkWeather = (latitude, longitude) => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=ef655657047c9741f85f1cfcf13c1ede`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      let tempValue = data["main"]["temp"];
+      console.log(`temp ${data["main"]["temp"]}`);
+      
+     document.querySelector('#temp').innerHTML = tempValue; 
+      
+    });
+};
